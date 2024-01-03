20e990cceac33e6a306401744532bf8a93e76663
ank809 <ankita51523@gmail.com>
2024-01-03 21:31:26 +0530

    connected to mongodb

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
-
+from flask_pymongo import PyMongo
 # instance of Flask
 app= Flask(__name__)
+app.config['SECRET_KEY']='d93aebd2b32fdbeae363ac3a'
+app.config['MONGO_URI']="mongodb+srv://ankita51523:<7WRwC3CPXmh5HHjB>@cluster0.gcgu6en.mongodb.net/?retryWrites=true&w=majority"
 
+# setup mongodb
+mongo_client= PyMongo(app)
+db= mongo_client.db
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -3,4 +3,4 @@ from application import app
 @app.route("/")
 
 def index():
-    return "<h1> Hello Ankita </h1>"
+    return "<h1> Hello Developers </h1>"
