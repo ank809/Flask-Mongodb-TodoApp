7451cb1ce4c283b79e022183463a8a4c4d835404
ank809 <ankita51523@gmail.com>
2024-01-04 13:43:15 +0530

    form created in add todo page

--- application/routes.py
+++ application/routes.py
@@ -4,11 +4,9 @@ from .forms import Todo_Form
 @app.route("/")
 
 def index():
-    # return render_template("layout.html", title="Layout Page")
-    return "Hello"
+    return render_template("view_todos.html", title="Layout Page")
 
-# @app.route("/add_todo")
-#
-# def add_todo():
-#     form =Todo_Form()
-#     return render_template("add_todo.html", form=form)
+@app.route("/add_todo")
+def add_todo():
+    form =Todo_Form()
+    return render_template("add_todo.html", form=form)
