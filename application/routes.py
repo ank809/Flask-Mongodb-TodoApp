624e52be2ff39ee52a77578131105aa1c33d1218
ank809 <ankita51523@gmail.com>
2024-01-07 18:08:38 +0530

    todo can be updated and deleted/completed

--- application/routes.py
+++ application/routes.py
@@ -1,16 +1,19 @@
 from flask import render_template, request, flash, redirect
-from application import app, db , mongo
+from application import app, db, mongo
 from .forms import TodoForm
 from datetime import datetime
+from bson import ObjectId
+
 
 @app.route("/")
 def get_todos():
-    todos=[]
+    todos = []
     for todo in db.todo.find().sort("date_created", -1):
-        todo["_id"]=str(todo["_id"])
+        todo["_id"] = str(todo["_id"])
         todo["date_created"] = todo["date_created"].strftime("%b %d %Y %H:%M:%S")
         todos.append(todo)
-    return render_template("view_todos.html",todos=todos)
+    return render_template("view_todos.html", todos=todos)
+
 
 @app.route("/add_todo", methods=['POST', 'GET'])
 def add_todo():
@@ -33,3 +36,37 @@ def add_todo():
     else:
         form = TodoForm()
     return render_template("add_todo.html", form=form)
+
+
+@app.route("/delete_todo/<id>")
+def delete_todo(id):
+    mongo.db.todo.find_one_and_delete({"_id": ObjectId(id)})
+    flash("Todo successfully deleted", "success")
+    return redirect("/")
+
+
+@app.route("/update_todo/<id>",  methods = ['POST', 'GET'])
+def update_todo(id):
+    if request.method == "POST":
+        form = TodoForm(request.form)
+        todo_name = form.name.data
+        todo_Desc = form.description.data
+        todo_completed = form.completed.data
+        mongo.db.todo.find_one_and_update({"_id": ObjectId(id)}, {"$set": {
+            "name": todo_name,
+            "description": todo_Desc,
+            "completed": todo_completed,
+            "datetime": datetime.utcnow()
+        }})
+        flash("Todo successfully updated", "success")
+        return redirect("/")
+    else:
+        form = TodoForm()
+
+        todo = mongo.db.todo.find_one_or_404({"_id": ObjectId(id)})
+        print(todo)
+        form.name.data = todo.get("name", None)
+        form.description.data = todo.get("description", None)
+        form.completed.data = todo.get("completed", None)
+
+    return render_template("add_todo.html", form = form)
